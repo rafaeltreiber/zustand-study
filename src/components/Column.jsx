a805a40561b7bac9b5dae98721a128a42e20091c
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2023-09-19 15:54:14 -0300

    first commit

---
+++ src/components/Column.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useStore } from "../store";
+import "./Column.css";
+import Task from "./Task";
+import classNames from "classnames";
+
+export default function Column({ state }) {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
+
+  const tasks = useStore((store) =>
+    store.tasks.filter((task) => task.state === state)
+  );
+
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+  const addTask = useStore((store) => store.addTask);
+  const moveTask = useStore((store) => store.moveTask);
+
+  return (
+    <div
+      className={classNames("column", { drop: drop })}
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDrop(false);
+        moveTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Add
+        </button>
+      </div>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+      {open && (
+        <div className="modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/store.js
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+const store = (set) => ({
+  tasks: [],
+  draggedTask: null,
+  addTask: (title, state) =>
+    set(
+      (store) => ({ tasks: [...store.tasks, { title, state }] }),
+      false,
+      "addTask"
+    ),
+  deleteTask: (title) =>
+    set(
+      (store) => ({
+        tasks: store.tasks.filter((task) => task.title !== title),
+      }),
+      false,
+      "deleteTask"
+    ),
+  setDraggedTask: (title) => set({ draggedTask: title }),
+  moveTask: (title, state) =>
+    set(
+      (store) => ({
+        tasks: store.tasks.map((task) =>
+          task.title === title ? { title, state } : task
+        ),
+      }),
+      false,
+      "moveTask"
+    ),
+});
+
+const log = (config) => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log(args);
+      set(...args);
+    },
+    get,
+    api
+  );
+
+export const useStore = create(
+  log(persist(devtools(store), { name: "store" }))
+);
